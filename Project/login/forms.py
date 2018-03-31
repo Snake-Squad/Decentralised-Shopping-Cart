726c47a9b1eda7a1bcccf3931be7c5fe8516a23e
philipyufei <philipyufei@hotmail.com>
2018-04-01 03:20:26 +1000

    added a small test and I can retrive data from webpage now

--- Project/login/forms.py
+++ Project/login/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
-class Login(forms.Form):
+class LoginForm(forms.Form):
     username = forms.CharField(label='username', max_length=40)
     password = forms.CharField(label='password', max_length=40)
     role = forms.CharField(label='role', max_length=1)
-    
--- Project/login/views.py
+++ Project/login/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
 from .models import LoginModel
+from .forms import LoginForm
 
 # Create your views here.
 def index(request):
@@ -14,21 +16,28 @@ def index(request):
     username = "admin"
     password = "createsuperuser"
     
+    # initialize data in loginModel
+    # data = LoginModel(username=None, password=None, role="B")
     # after getting username and password, we need to check its validation
     ml = LoginModel()
-    role = ml.check_valid(username, password)
-    print(" ------> ", role, " <--------")
-    #role = 'B'
+    #role = ml.check_valid(username, password)
+    role = None
+    # print(" ------> ", role, " <--------")
 
+    form = LoginForm(request.POST)
+    #form = LoginForm()
+    if form.is_valid():
+      print("form is good !!!!! ")
+      print(request.POST['username'])
+      print()
+    else:
+      print("form is bad T^T")
     if role is not None:
       return HttpResponseRedirect('/')
     else:
-      return render(request, 'login/index.html')
+      return render(request, 'login/test.html', {'form': form})
   else:
     print("        ----------- in Get -----------          ")
-    # initialize data in loginModel
-    # data = LoginModel(username=None, password=None, role="B")
-    return render(
-      request, 
-      'login/index.html'
-    )
+    form = LoginForm()
+    #return render(request, 'login/index.html')
+    return render(request, 'login/test.html', {'form': form})
