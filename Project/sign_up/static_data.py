7e5a412f26d7b84497030a054ed5c61f3d8dbd29
SURFACE-Y\R&Y <minzhenye@126.com>
2018-04-05 18:14:24 +1000

    update sign_up

--- Project/sign_up/static_data.py
+++ Project/sign_up/static_data.py
@@ -13,3 +13,19 @@ states={
     ('Western Australia', 'WA')
   ]
 }
+'''
+question1={
+  'Question_list':
+  [
+	  ('Question1', 'what is your favourite animal?'),
+	  ('Question2', 'what is your favourite food?'),
+	  ('Question3', 'what is your favourite fruit?')
+  ]
+}
+'''
+question=[
+  ('what is your favourite animal?', 'question1'),
+  ('what is your favourite food?', 'question2'),
+  ('what is your favourite fruit?','question3')
+  
+]
--- Project/sign_up/views.py
+++ Project/sign_up/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import PostInfo
-from .forms import PersonalInfo
+from .models import *
+from .forms import *
+
+#from .models import PostInfo
+#from .forms import PersonalInfo
 
 # Create your views here.
 def index(request):
-  if request.method == "GET":
-    info = PersonalInfo()
-    return render(request, 'sign_up/index.html',
-      {'info': info})
-  else:
-    print(" --------------- in post ----------------- ")
-    model = PostInfo()
-    info = PersonalInfo(request.POST)
-    if info.is_valid():
-      model.post_info(request)
-    else:
-      print('info is invalid')
-      
-    return render(request, 'sign_up/index.html',
-      {'info': info})
+	print("index------------------------------")
+	if request.method == "GET":
+		print("--------------------------------------------get")
+		info = PersonalInfo()
+		question = SecurityQuestion()
+		return render(request, 'sign_up/index.html',
+		  {'info': info,'question':question})
+	else:
+		print(" --------------- in post ----------------- ")
+		model = PostInfo()
+		info = PersonalInfo(request.POST)
+		model_question = PostSecurity()
+		seq_info = SecurityQuestion(request.POST)
+		print("info--------------------",info)
+		if info.is_valid():
+		  model.post_info(request)
+		else:
+		  print('info is invalid')
+		
+		
+		print("seq_info--------------------",seq_info)
+		if seq_info.is_valid():
+		  model_question.post_security(request)
+		else:
+		  print('seq_info is invalid')
+		  
+		return render(request, 'sign_up/index.html',
+		  {'info': info,'seq_info':seq_info})
