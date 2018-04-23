3579808c820a5b915cf83ee3a747a6d7df6ed2f9
SURFACE-Y\R&Y <minzhenye@126.com>
2018-04-23 20:50:28 +1000

    change the sign up page and store the user sign up information to firebase

---
+++ PetShop/src/static/js/signup.js
@@ -0,0 +1,58 @@
+function checkSignUp() {
+  //  Initialize Firebase
+  
+  var config = {
+    apiKey: "AIzaSyBzvcZDres2eUAUX6PBHRlo858ftMznDKs",
+    authDomain: "comp9900-4b79d.firebaseapp.com",
+    databaseURL: "https://comp9900-4b79d.firebaseio.com",
+    projectId: "comp9900-4b79d",
+    storageBucket: "comp9900-4b79d.appspot.com",
+    messagingSenderId: "445311599888"
+  };
+  firebase.initializeApp(config);
+  alert("hhd");
+  
+  // get information from the webpage
+  var username_ = document.getElementById("email").value.trim();
+  var password_ = document.getElementById("psw").value.trim();
+  var confirm_password = document.getElementById("psw-repeat").value.trim();
+  //var remember = document.getElementById("remember").value.trim();
+  var buyer = document.getElementById("buyer").value;
+  var seller = document.getElementById("seller").value;
+  alert(buyer);
+  if(password_!=confirm_password)
+  {
+      alert("password is not same");
+  }
+  else{
+    alert("all right");
+  }
+ 
+  //get the database
+  //var users = firebase.database().ref().child('users').push({email:"yy",password:"uu",role:"zz" });
+  if(buyer == "on")
+  {
+      var users = firebase.database().ref().child('users').push({email:username_,password:password_,role:"buyer"});
+  }
+  else{
+      
+    var users = firebase.database().ref().child('users').push({email:username_,password:password_,role:"seller"});
+  }
+  alert(users);
+  
+  
+  // This must be the last function of all
+  //users.on("value", function(snapshot) {
+    
+    
+   // );
+
+   // alert(isValid);
+    //if (isValid) {
+    //  window.location.replace("/");
+   // } else {
+   //   window.location.replace("/");
+   // }
+  //});
+
+}
