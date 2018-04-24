7f01c180f5fe618799e02817c8663db664293168
Vijay <qq471849587@gmail.com>
2018-04-24 11:57:47 +1000

    bug fixed

--- PetShop/src/static/js/navbar.js
+++ PetShop/src/static/js/navbar.js
@@ -6,6 +6,11 @@ function onClickLogin() {
 }
 
 function searchPet(){
+  var wat = document.getElementById("searchPetsRow");
+  console.log("wat", wat);
+  wat.innerHTML = "";
+  console.log("wat2", wat);
+  
   var input = document.getElementById("searchInput").value.toLowerCase();
   console.log(input);
   var searchP=[];
@@ -15,7 +20,7 @@ function searchPet(){
     var searchP = data.filter(e => e.name.toLowerCase().includes(input) || e.breed.toLowerCase().includes(input)||e.location.toLowerCase().includes(input));
     //将这个serchP里的元素在index页面里显示出来，或者替换之前循环显示的那个list
     var petsRow = document.getElementById("petsRow");
-    var searchPetsRow =$("#searchPetsRow")
+    var searchPetsRow =$("#searchPetsRow");
     var petTemplate = $("#petTemplate");
     petsRow.style.display = "none";
     for (i = 0; i < searchP.length; i ++) {
@@ -28,6 +33,7 @@ function searchPet(){
       petTemplate.find('.btn-add').attr('data-id', searchP[i].id);
       searchPetsRow.append(petTemplate.html());
     }
+    console.log(searchPetsRow);
   });
 }
 
