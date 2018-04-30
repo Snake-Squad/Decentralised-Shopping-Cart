0b2f5995d5b0eaaef37d74415e30e1093ed095e1
SURFACE-Y\R&Y <minzhenye@126.com>
2018-04-30 17:22:03 +1000

    edit personInfo to show person information

--- PetShop/src/static/js/navbar.js
+++ PetShop/src/static/js/navbar.js
@@ -16,7 +16,7 @@ function onClickCart() {
 
 
 /* functions after logging in */
-function onClickPorfile() { 
+function onClickProfile() { 
   window.location.href="personInfo.html";
 }
 
@@ -34,6 +34,10 @@ function onClickTransactions(){
 
 function onClickLogOut() {
   delete_cookie("userName"); 
+}
+
+function editPersonInfo(){
+  window.location.href="editPerson.html";
 }
 
 function delete_cookie(name) {
@@ -104,6 +108,77 @@ function getCookieValue(name) {
 }
 
 window.onload = function() {  
+alert("kkkkk");
+alert(window.location.href);
+ if(window.location.href.indexOf("personInfo.html")!=-1){
+     alert("jjjj");
+        var userNameValue = getCookieValue("userName");  
+        alert(userNameValue);
+        if(userNameValue === undefined || userNameValue == null || userNameValue.length <= 0){
+            document.getElementById("login_navabar").text = "Login"; 
+            
+            }
+        else{
+          document.getElementById("loginnavbarDropdown").text = userNameValue;  
+          document.getElementById("loginnavbarDropdown").style.display = 'block';
+          document.getElementById("login_navabar").style.display = 'none';
+          document.getElementById("signup_navabar").style.display = 'none';
+          var config = {
+            apiKey: "AIzaSyBzvcZDres2eUAUX6PBHRlo858ftMznDKs",
+            authDomain: "comp9900-4b79d.firebaseapp.com",
+            databaseURL: "https://comp9900-4b79d.firebaseio.com",
+            projectId: "comp9900-4b79d",
+            storageBucket: "comp9900-4b79d.appspot.com",
+            messagingSenderId: "445311599888"
+          };
+          firebase.initializeApp(config);
+
+          // get user's email and password from the webpage
+          var userNameValue = getCookieValue("userName"); 
+          alert("lllll");
+          //alert(userNameValue);
+          // get the database
+          var users = firebase.database().ref().child('users');
+
+          
+          // This must be the last function of all
+          users.on("value", function(snapshot) {
+            var isValid = false;
+            var userId = "";            // this id is used in blockchain
+
+            snapshot.forEach(function(user) {
+              // retrieve data from db
+              var userKey = user.key;   // user id
+              var userVal = user.val(); // user's info (email, fn, ln, pw, role)
+              
+              // check whether it matches or not
+              if (userVal.email == userNameValue) {
+             
+                  isValid = true;
+
+                  document.getElementById("email").innerHTML=userNameValue;
+                  alert(userVal);
+                  document.getElementById("first_name").innerHTML=userVal.first_name;
+                  document.getElementById("last_name").innerHTML=userVal.last_name;
+                  alert(userVal.last_name);
+                  document.getElementById("address1").innerHTML=userVal.street;
+                  document.getElementById("address2").innerHTML=userVal.suite;
+                  document.getElementById("country").innerHTML=userVal.country;
+                  document.getElementById("state").innerHTML=userVal.state;
+                  document.getElementById("zip").innerHTML=userVal.zip;
+                  
+                  return true;          // break the loop
+                
+                
+              }
+            });
+          });
+          
+      
+        }
+        
+ }
+ else{
     var value = getCookieValue("userName");  
     console.log(value);
     if(value === undefined || value == null || value.length == 0) {
@@ -114,4 +189,5 @@ window.onload = function() {
         document.getElementById("login_navabar").style.display = 'none';
         document.getElementById("signup_navabar").style.display = 'none';
     }
+ }
 }
