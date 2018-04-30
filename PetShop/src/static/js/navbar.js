082d85c1aeba9e17d96cdb855ca10aaabb25746e
philipyufei <philipyufei@hotmail.com>
2018-04-30 22:37:30 +1000

    Recharge is good to use now

--- PetShop/src/static/js/navbar.js
+++ PetShop/src/static/js/navbar.js
@@ -1,89 +1,98 @@
-/* functions while not logged in */
+/* -----------------------------------------------------------------------------
+ * functions while not logged in 
+ * -----------------------------------------------------------------------------
+ */
 function onClickLogin() {
-  window.location.href="login.html";
+    window.location.href="login.html";
 }
 
 function onClickSignUp() {
-  alert("Sign Up");
-  window.location.href="signup.html";
+    alert("Sign Up");
+    window.location.href="signup.html";
 }
 
-//shop cart 
 function onClickCart() {
-  alert("Cart");
-  window.location.href="cart.html?"+"txt="+encodeURI(App.petInCart);
+    alert("Cart");
+    window.location.href="cart.html?"+"txt="+encodeURI(App.petInCart);
 }
 
 
-/* functions after logging in */
+/* -----------------------------------------------------------------------------
+ * functions after logging in 
+ * -----------------------------------------------------------------------------
+ */
 function onClickProfile() { 
-  window.location.href="personInfo.html";
+    window.location.href="personInfo.html";
 }
 
 function onClickAddAPuppy(){
-  window.location.href="addpuppy.html";
+    window.location.href="addpuppy.html";
 }
 
 function onClickRecharge(){
-  window.location.href="recharge.html";
+    window.location.href="recharge.html";
 }
 
 function onClickTransactions(){
-  window.location.href="transaction.html";
+    window.location.href="transaction.html";
 }
 
 function onClickLogOut() {
-  delete_cookie("userName"); 
+    delete_cookie("userName"); 
 }
 
 function editPersonInfo(){
-  window.location.href="editPerson.html";
+    window.location.href="editPerson.html";
 }
 
-function delete_cookie(name) {
-  var exp = new Date();  
-  exp.setTime(exp.getTime() - 1);  
-  var cval = getCookieValue(name);  
-  if (cval != null) {      
-    document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();  
-  }
-  alert("delete cookie");
-  window.location.replace("/");
-}
 
-/* search function */
-function searchPet(){
-  var wat = document.getElementById("searchPetsRow");
-  console.log("wat", wat);
-  wat.innerHTML = "";
-  console.log("wat2", wat);
+
+/* -----------------------------------------------------------------------------
+ * search function 
+ * This function is really bad now.
+ * -----------------------------------------------------------------------------
+ */
+function searchPet() {
+    alert("onClick Search");
+    var wat = document.getElementById("searchPetsRow");
+    console.log("wat", wat);
+    wat.innerHTML = "";
+    console.log("wat2", wat);
   
-  var input = document.getElementById("searchInput").value.toLowerCase();
-  console.log(input);
-  var searchP=[];
-  //取出pets json中的数据，然后循环item，如果其中的name或者品种中的string包含
-  //这个formValue，就存入一个新的list，然后显示这个新的list
-  $.getJSON('../pets.json', function(data) {
-    var searchP = data.filter(e => e.name.toLowerCase().includes(input) || e.breed.toLowerCase().includes(input)||e.location.toLowerCase().includes(input));
+    var input = document.getElementById("searchInput").value.toLowerCase();
+    console.log(input);
+    var searchP=[];
+    //取出pets json中的数据，然后循环item，如果其中的name或者品种中的string包含
+    //这个formValue，就存入一个新的list，然后显示这个新的list
+    $.getJSON('../pets.json', function(data) {
+    var searchP = data.filter(e => e.name.toLowerCase().includes(input) ||
+        e.breed.toLowerCase().includes(input)||
+        e.location.toLowerCase().includes(input)
+    );
     //将这个serchP里的元素在index页面里显示出来，或者替换之前循环显示的那个list
     var petsRow = document.getElementById("petsRow");
     var searchPetsRow =$("#searchPetsRow");
     var petTemplate = $("#petTemplate");
     petsRow.style.display = "none";
     for (i = 0; i < searchP.length; i ++) {
-      petTemplate.find('.panel-title').text(searchP[i].name);
-      petTemplate.find('img').attr('src', searchP[i].picture);
-      petTemplate.find('.pet-breed').text(searchP[i].breed);
-      petTemplate.find('.pet-age').text(searchP[i].age);
-      petTemplate.find('.pet-location').text(searchP[i].location);
-      // petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-      petTemplate.find('.btn-add').attr('data-id', searchP[i].id);
-      searchPetsRow.append(petTemplate.html());
+        petTemplate.find('.panel-title').text(searchP[i].name);
+        petTemplate.find('img').attr('src', searchP[i].picture);
+        petTemplate.find('.pet-breed').text(searchP[i].breed);
+        petTemplate.find('.pet-age').text(searchP[i].age);
+        petTemplate.find('.pet-location').text(searchP[i].location);
+        // petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+        petTemplate.find('.btn-add').attr('data-id', searchP[i].id);
+        searchPetsRow.append(petTemplate.html());
     }
     console.log(searchPetsRow);
   });
 }
 
+
+/* -----------------------------------------------------------------------------
+ * Deal with cookies. Add, get and delete
+ * -----------------------------------------------------------------------------
+ */
 function addCookie(name, username, uid, days, path) {
     var name = escape(name);  
     var username = escape(username);  
@@ -123,88 +132,13 @@ function getCookieValue(name) {
     } 
 }
 
+function delete_cookie(name) {
+    var exp = new Date();  
+     exp.setTime(exp.getTime() - 1);  
+     var cval = getCookieValue(name);  
+    if (cval != null) {      
+        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();  
+    }
+    window.location.replace("/");
+}
 
-// window.onload = function() {  
-// alert("kkkkk");
-// alert(window.location.href);
- // if(window.location.href.indexOf("personInfo.html")!=-1){
-     // alert("jjjj");
-        // var userNameValue = getCookieValue("userName");  
-        // alert(userNameValue);
-        // if(userNameValue === undefined || userNameValue == null || userNameValue.length <= 0){
-            // document.getElementById("login_navabar").text = "Login"; 
-            
-            // }
-        // else{
-          // document.getElementById("loginnavbarDropdown").text = userNameValue;  
-          // document.getElementById("loginnavbarDropdown").style.display = 'block';
-          // document.getElementById("login_navabar").style.display = 'none';
-          // document.getElementById("signup_navabar").style.display = 'none';
-          // var config = {
-            // apiKey: "AIzaSyBzvcZDres2eUAUX6PBHRlo858ftMznDKs",
-            // authDomain: "comp9900-4b79d.firebaseapp.com",
-            // databaseURL: "https://comp9900-4b79d.firebaseio.com",
-            // projectId: "comp9900-4b79d",
-            // storageBucket: "comp9900-4b79d.appspot.com",
-            // messagingSenderId: "445311599888"
-          // };
-          // firebase.initializeApp(config);
-
-          // // get user's email and password from the webpage
-          // var userNameValue = getCookieValue("userName"); 
-          // alert("lllll");
-          // //alert(userNameValue);
-          // // get the database
-          // var users = firebase.database().ref().child('users');
-
-          
-          // // This must be the last function of all
-          // users.on("value", function(snapshot) {
-            // var isValid = false;
-            // var userId = "";            // this id is used in blockchain
-
-            // snapshot.forEach(function(user) {
-              // // retrieve data from db
-              // var userKey = user.key;   // user id
-              // var userVal = user.val(); // user's info (email, fn, ln, pw, role)
-              
-              // // check whether it matches or not
-              // if (userVal.email == userNameValue) {
-             
-                  // isValid = true;
-
-                  // document.getElementById("email").innerHTML=userNameValue;
-                  // alert(userVal);
-                  // document.getElementById("first_name").innerHTML=userVal.first_name;
-                  // document.getElementById("last_name").innerHTML=userVal.last_name;
-                  // alert(userVal.last_name);
-                  // document.getElementById("address1").innerHTML=userVal.street;
-                  // document.getElementById("address2").innerHTML=userVal.suite;
-                  // document.getElementById("country").innerHTML=userVal.country;
-                  // document.getElementById("state").innerHTML=userVal.state;
-                  // document.getElementById("zip").innerHTML=userVal.zip;
-                  
-                  // return true;          // break the loop
-                
-                
-              // }
-            // });
-          // });
-          
-      
-        // }
-        
- // }
- // else{
-    // var value = getCookieValue("userName");  
-    // console.log(value);
-    // if(value === undefined || value == null || value.length == 0) {
-        // document.getElementById("login_navabar").text = "Login";     
-    // } else {
-        // document.getElementById("loginnavbarDropdown").text = value[0];  
-        // document.getElementById("loginnavbarDropdown").style.display = 'block';
-        // document.getElementById("login_navabar").style.display = 'none';
-        // document.getElementById("signup_navabar").style.display = 'none';
-    // }
- // }
-// }
---
+++ PetShop/src/static/js/recharge.js
@@ -0,0 +1,103 @@
+Controller = {
+    web3Provider: null,
+    contracts: {},
+    username: null,
+    userId: null,
+    amount: null,
+
+    initWeb3: function() {
+        // Is there an injected web3 instance?
+        if (typeof web3 !== 'undefined') {
+            Controller.web3Provider = web3.currentProvider;
+        } else {
+            // If no injected web3 instance is detected, fall back to Ganache
+            Controller.web3Provider = new Web3.providers.HttpProvider(
+                'http://localhost:7545');
+        } 
+        web3 = new Web3(Controller.web3Provider);
+        return Controller.initContract();
+    },
+
+    initContract: function() {
+        $.getJSON('Controller.json', function(data) {
+            // Get the necessary contract artifact file
+            var ControllerArtifact = data;
+            console.log(data);
+            // Instantiate it with truffle-contract
+            Controller.contracts.Controller = 
+                TruffleContract(ControllerArtifact);
+            // Set the provider for our contract
+            Controller.contracts.Controller.setProvider(
+                Controller.web3Provider);
+            return Controller.showBalance();
+        });
+    },
+
+    showBalance: function() {
+        var ControllerInstance;
+        Controller.contracts.Controller.deployed().then(function(instance) {
+        ControllerInstance = instance;
+            return ControllerInstance.getAccount(Controller.userId);
+        }).then(function(result) {
+            console.log("stored on block chain:", result);
+            $("#userEmail").html(Controller.username);
+            $("#userBalance").html("Balance: $" + result[2]["c"][0]);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    recharge: function() {
+        var userId = Controller.userId;
+        var amount = Controller.amount;
+        console.log(userId, amount);
+        // -------------------------------
+        web3.eth.getAccounts(function(error, accounts) {
+            if (error) {
+                console.log(error);
+            } else {
+                var account = accounts[0];
+                Controller.contracts.Controller.deployed()
+                    .then(function(instance) {
+                        return instance.recharge(userId, amount, {gas:3000000});
+                }).then(function(result) {
+                    console.log("After recharge:", result);
+                    return Controller.refresh();
+                }).catch(function(err) {
+                    console.log(err.message);
+                });
+            }
+        });
+    },
+
+    refresh: function() {
+        alert("check");
+        location.reload();
+    }
+};
+
+
+// While Recharge Button is clicked
+$("#rechargeButton").click(function() {
+    //Controller.recharge("0x2c6315b775d00007935b3760af5f48f0a9f5a96a", $("#rechargeInputBuyer").val(), {gas:3000000});
+    Controller.amount = $("#rechargeInput").val();
+    console.log(Controller.username, Controller.userId, Controller.amount);
+    Controller.recharge();
+});
+
+// get cookie while on this page
+window.onload = function() {  
+    var value = getCookieValue("userName");  
+    console.log(value);
+    if (value === undefined || value == null || value.length == 0) {
+        document.getElementById("login_navabar").text = "Login"; 
+    } else {
+        Controller.username = value[0];
+        Controller.userId = value[1];
+        Controller.initWeb3();
+        document.getElementById("loginnavbarDropdown").text = value[0];  
+        document.getElementById("loginnavbarDropdown").style.display = 'block';
+        document.getElementById("login_navabar").style.display = 'none';
+        document.getElementById("signup_navabar").style.display = 'none';
+    }
+}
