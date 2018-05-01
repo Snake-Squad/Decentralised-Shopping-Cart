1640d7bb9995ed1cb36c7acdd2a80918be041a81
philipyufei <philipyufei@hotmail.com>
2018-05-02 00:40:18 +1000

    Add a Puppy to Block Chain is almost done.

--- PetShop/src/static/js/add_a_puppy.js
+++ PetShop/src/static/js/add_a_puppy.js
@@ -1,9 +1,13 @@
 Controller = {
     web3Provider: null,
     contracts: {},
-    username: null,
-    userId: null,
-    amount: null,
+    sellerId: null,
+    name: null,
+    breed: null,
+    age: null,
+    birthPlace: null,
+    price: null,
+    image: null,
 
     initWeb3: function() {
         // Is there an injected web3 instance?
@@ -29,51 +33,94 @@ Controller = {
             // Set the provider for our contract
             Controller.contracts.Controller.setProvider(
                 Controller.web3Provider);
-            //return Controller.showBalance();
+            return Controller.addAPuppy();
         });
     },
 
-    // showBalance: function() {
-    //     var ControllerInstance;
-    //     Controller.contracts.Controller.deployed().then(function(instance) {
-    //     ControllerInstance = instance;
-    //         return ControllerInstance.getAccount(Controller.userId);
-    //     }).then(function(result) {
-    //         console.log("stored on block chain:", result);
-    //         $("#userEmail").html(Controller.username);
-    //         $("#userBalance").html("Balance: $" + result[2]["c"][0]);
-    //     }).catch(function(err) {
-    //         console.log(err.message);
-    //     });
-    // },
+    addAPuppy: function() {
+        var puppyId = generateAddress();
+        console.log(puppyId, Controller.sellerId, 
+            Controller.name, Controller.breed, Controller.age, 
+            Controller.birthPlace, Controller.price, Controller.image
+        );
 
-    // recharge: function() {
-    //     var userId = Controller.userId;
-    //     var amount = Controller.amount;
-    //     console.log(userId, amount);
-    //     // -------------------------------
-    //     web3.eth.getAccounts(function(error, accounts) {
-    //         if (error) {
-    //             console.log(error);
-    //         } else {
-    //             var account = accounts[0];
-    //             Controller.contracts.Controller.deployed()
-    //                 .then(function(instance) {
-    //                     return instance.recharge(userId, amount, {gas:3000000});
-    //             }).then(function(result) {
-    //                 console.log("After recharge:", result);
-    //                 return Controller.refresh();
-    //             }).catch(function(err) {
-    //                 console.log(err.message);
-    //             });
-    //         }
-    //     });
-    // },
+        // ---------------------------------------------------------------------
+        web3.eth.getAccounts(function(error, accounts) {
+            if (error) {
+                console.log(error);
+            } else {
+                var account = accounts[0];
+                Controller.contracts.Controller.deployed()
+                    .then(function(instance) {
+                        return instance.addPuppy(
+                            puppyId,
+                            Controller.sellerId,
+                            Controller.name,
+                            Controller.breed,
+                            Controller.age,
+                            Controller.birthPlace,
+                            Controller.price,
+                            Controller.image, 
+                            {gas:3000000}
+                        );
+                }).then(function(result) {
+                    console.log("Successfully Added to Block Chain.", result);
+                    return Controller.refresh();
+                }).catch(function(err) {
+                    console.log(err.message);
+                });
+            }
+        });
+    },
 
     refresh: function() {
         location.reload();
     }
 };
+
+
+// before adding a puppy, check the valiation of all inputs varables.
+// None of the inputs can be empty.
+function checkValidation() {
+    // retrieve data from UI
+    var name = $("#inputName").val();
+    var breed = $("#inputBreed").val();
+    var age = parseInt($("#inputAge").val());
+    var birthPlace = $("#inputBirthPlace").val();
+    var price = parseInt($("#inputPrice").val());
+    var image = $("#imgOfPuppy").attr("src");
+    console.log(name, breed, age, birthPlace, price, image);
+
+    // TODO: Check validation of each input
+    var isValid = false;
+
+
+
+    isValid = true; // this is only for testing
+    
+    // if all inputs are valid, add the puppy to block chain
+    if (isValid) {
+        Controller.name = name;
+        Controller.breed = breed;
+        Controller.age = age;
+        Controller.birthPlace = birthPlace;
+        Controller.price = price;
+        Controller.image = image;
+        Controller.initWeb3();
+    }
+
+}
+
+
+// While Confrim Button is clicked
+$("#btnUploadImg").click(function() {
+    alert("Upload Image")
+});
+
+// While Confrim Button is clicked
+$("#btnConfirm").click(function() {
+    checkValidation();
+});
 
 
 // Once the page is load run this
@@ -83,9 +130,7 @@ window.onload = function() {
     if (value === undefined || value == null || value.length == 0) {
         document.getElementById("login_navabar").text = "Login"; 
     } else {
-        //Controller.username = value[0];
-        //Controller.userId = value[1];
-        //Controller.initWeb3();
+        Controller.sellerId = value[1];
         document.getElementById("loginnavbarDropdown").text = value[0];  
         document.getElementById("loginnavbarDropdown").style.display = 'block';
         document.getElementById("login_navabar").style.display = 'none';
--- PetShop/src/static/js/generators.js
+++ PetShop/src/static/js/generators.js
@@ -1,8 +1,8 @@
 function generateAddress() {
-  var userId = "0x";
+  var address = "0x";
   var chars = "abcdef0123456789";
   for (var i = 0; i < 40; i++) {
-    userId += chars.charAt(Math.floor(Math.random() * chars.length));
+    address += chars.charAt(Math.floor(Math.random() * chars.length));
   }
-  return userId;
+  return address;
 }
