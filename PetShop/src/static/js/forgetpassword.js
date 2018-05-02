ba308db59fe0ba5818e1c623378bf4f5a97a4b2c
Vijay <qq471849587@gmail.com>
2018-05-03 03:55:51 +1000

    change

--- PetShop/src/static/js/forgetpassword.js
+++ PetShop/src/static/js/forgetpassword.js
@@ -14,7 +14,7 @@ function onClickForget() {
   //window.location.href="index.html";
   // get user's email and password from the webpage
   var username = document.getElementById("username").value;
-  //var password = document.getElementById("password").value;
+  
   if(username.length==0)
   {
       alert("Username cannot be empty, please input");
@@ -24,32 +24,34 @@ function onClickForget() {
   var users = firebase.database().ref().child('users');
   //alert(users);
 
-  // This must be the last function of all
+  // // This must be the last function of all
+  var isValid = true;
   users.on("value", function(snapshot) {
     var isValid = false;
+    console.log("---")
 
     snapshot.forEach(function(user) {
       // retrieve data from db
       var userKey = user.key; // user id
       var userVal = user.val(); // user's info (email, fn, ln, pw, role)
       
-      //alert(userVal.email);
-      // check whether it matches or not
-      if (userVal.email == username) {
-        //if (userVal.password == password) {
+      alert(userVal.email);
+      //check whether it matches or not
+      if (userVal.email == username) {       
           isValid = true;
           return true;
-        //}
       }
+      });
+    alert(isValid);
     });
-    //alert(isValid);
+    
     if (isValid) {
       window.location.replace("forget_password.html");
     } else {
       alert("Username doesn't exist, please input again");
       window.location.href="login.html";
     }
-  });
+  
   
 
 }
