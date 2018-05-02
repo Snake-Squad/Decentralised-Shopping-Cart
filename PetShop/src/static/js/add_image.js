f40bc96e042a50bb5d8a1713b587c0193e418e2c
MSI\yanjie <kelseyan0305@gmail.com>
2018-05-02 11:40:51 +1000

    add upload image part into add_puppy

---
+++ PetShop/src/static/js/add_image.js
@@ -0,0 +1,13 @@
+function uploadimage(){
+        const link = document.getElementById('image');
+        const client = filestack.init('Ac3EdiggQlOxJvyyxsfTmz');
+        client.pick({
+        accept:'image/*',
+        maxFiles:1,
+        imageDim:[400,300]
+        }).then(function(result) {
+  const fileUrl = result.filesUploaded[0].url;
+  link.href=fileUrl;
+  link.src=fileUrl;
+});
+}
