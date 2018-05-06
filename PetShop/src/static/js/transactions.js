53e98aa922daa841b6b525edf7de4309d80200e9
philipyufei <philipyufei@hotmail.com>
2018-05-06 17:29:46 +1000

    update trasactions UI

---
+++ PetShop/src/static/js/transactions.js
@@ -0,0 +1,9 @@
+$("#transTemplate").click(function() {
+    // console.log($("#transDetail").css());
+    $("#transDetail").css("display", "block");
+});
+
+
+$("#close").click(function() {
+    $("#transDetail").css("display", "none");
+});
