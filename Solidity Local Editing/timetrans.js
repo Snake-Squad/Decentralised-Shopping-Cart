979a00dde5137ca0a5b7b9167f8b1e0bccb7c628
MSI\yanjie <kelseyan0305@gmail.com>
2018-05-06 20:06:06 +1000

    timeTransfer

---
+++ Solidity Local Editing/timetrans.js
@@ -0,0 +1,33 @@
+function formatSeconds(value) {
+    var theTime = parseInt(value);// 需要转换的时间秒
+    var theTime1 = 0;// 分
+    var theTime2 = 0;// 小时
+    var theTime3 = 0;// 天
+    if(theTime > 60) {
+        theTime1 = parseInt(theTime/60);
+        theTime = parseInt(theTime%60);
+        if(theTime1 > 60) {
+            theTime2 = parseInt(theTime1/60);
+            theTime1 = parseInt(theTime1%60);
+            if(theTime2 > 24){
+                //大于24小时
+                theTime3 = parseInt(theTime2/24);
+                theTime2 = parseInt(theTime2%24);
+            }
+        }
+    }
+    var result = '';
+    if(theTime > 0){
+        result = ":"+parseInt(theTime);
+    }
+    if(theTime1 > 0) {
+        result = ":"+parseInt(theTime1)+result;
+    }
+    if(theTime2 > 0) {
+        result = ":"+parseInt(theTime2)+result;
+    }
+    if(theTime3 > 0) {
+        result = ""+parseInt(theTime3)+"days "+result;
+    }
+    return result;
+}
