708e32150d7e9b5b4cc80ec43381b5606577a3f8
philipyufei <philipyufei@hotmail.com>
2018-05-15 02:04:50 +1000

    Update

--- PetShop/src/static/js/upload_an_image.js
+++ PetShop/src/static/js/upload_an_image.js
@@ -1,11 +1,11 @@
 // While Confrim Button is clicked
 $("#btnUploadImg").click(function() {
     const link = document.getElementById('imgOfPuppy');
-    const client = filestack.init('Ac3EdiggQlOxJvyyxsfTmz');
+    const client = filestack.init('AYIEV06KGT6yp68IaZPzVz');
     client.pick({
         accept:'image/*',
         maxFiles:1,
-        imageDim:[480,360]
+        imageDim:[450,450]
     }).then(function(result) {
         const fileUrl = result.filesUploaded[0].url;
         link.href=fileUrl;
